4c2e002ef42eff36e93c74e1805117f98152cffd
Peter Levinsky <pele@easj.dk>
2016-11-16 13:28:45 +0100

    Burde være i en lavere sdk pakke

---
+++ DemoObserverPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Start();
+        }
+
+        private void Start()
+        {
+            observable ob = new observable();
+            ob.PropertyChanged += ObserverMetode1;
+            ob.Name = "peter";
+
+            ob.PropertyChanged += ObserverMetode2;
+            ob.Number = 12345;
+
+            Console.ReadLine();
+        }
+
+        private void ObserverMetode2(object sender, PropertyChangedEventArgs e)
+        {
+            Console.WriteLine("Property derer ændret er " + e.PropertyName);
+        }
+
+        private void ObserverMetode1(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            Console.WriteLine($"Metode 1 ob = " + sender.ToString());
+            
+        }
+
+        public Program()
+        {
+        }
+
+
+    }
+}
---
+++ DemoObserverPattern/observable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoObserverPattern
+{
+    class observable:INotifyPropertyChanged
+    {
+        private int number;
+        private String name;
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                OnPropertyChanged();   
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public observable()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Number: {number}, Name: {name}";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
